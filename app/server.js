0996d1d3b1fab05de183b56ce7c60bcada9e0a93
Your Name <you@example.com>
2023-12-08 10:20:40 +0400

    db package added

--- app/server.js
+++ app/server.js
@@ -1,6 +1,19 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+const pg = require('pg');
+
+let requests = {
+    "list": (res)=>{
+        res.statusCode = 200;
+        res.end(JSON.stringify(["1", "2", "3"]));
+    },
+
+    "add": (res, args){
+        res.statusCode = 200;
+
+    }
+}
 
 http.createServer((req, res) => {
     if (req.url == "/") {
@@ -8,11 +21,19 @@ http.createServer((req, res) => {
         return;
     }
 
+    if (req.url.startsWith("/api/")) {
+        let command = req.url.substring(5).split("?");
+
+        requests[command[0]](res, command[1]);
+
+        return;
+    }
+
     let p = path.resolve('.' + req.url);
 
     if (fs.existsSync(p))
         res.end(fs.readFileSync(p));
-    else{
+    else {
         res.statusCode = 404;
         res.end("NOT FOUND");
     }
