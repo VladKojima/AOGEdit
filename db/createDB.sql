94cebb19d8873c7036b9032a9ee4015b9a188097
Your Name <you@example.com>
2023-12-15 14:41:49 +0400

    addDesc to Saves in DB

--- db/createDB.sql
+++ db/createDB.sql
@@ -1,7 +1,9 @@
 CREATE TABLE public.saves
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    CONSTRAINT saves_pkey PRIMARY KEY (id)
+    title character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT saves_pkey PRIMARY KEY (id),
+    CONSTRAINT utitle UNIQUE ("title")
 );
 
 CREATE TABLE public.events
